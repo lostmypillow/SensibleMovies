e63e53d48c14f396aa7810a1dd95777550e799ff
lostmypillow <47119111+lostmypillow@users.noreply.github.com>
2024-03-21 13:52:56 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,19 +1,7 @@
 'use client'
 import { useState } from "react";
 
-function MovieCard({ movie }) {
-  return (
-    <div className="flex min-w-80 max-h-[500px] flex-col items-center justify-center border-zinc-200 border-2 shadow-2xl rounded-lg m-3 p-3 backdrop-blur-md">
-      <img
-        src={movie.Poster != "N/A" ? movie.Poster : "{ SearchIcon }"}
-        alt="poster"
-        className="max-w-fit max-h-[300px]"
-      />
-      <span>{movie.Type.toUpperCase()}</span>
-      <h3 className="text-xl">{movie.Title} ({movie.Year})</h3>
-    </div>
-  );
-}
+
 
 export default function App() {
   const [movies, setMovies] = useState([]);
@@ -118,11 +106,9 @@ export default function App() {
       </div>
 
       <div className="flex flex-col md:flex-row flex-wrap gap-4 md:gap-0 mt-4">
-        {movies?.length > 0 ? (
-          movies.map((movie) => <MovieCard movie={movie} />)
-        ) : (
-          <h1>no movies found</h1>
-        )}
+      
+          <h1>no movies found next</h1>
+      ur
       </div>
     </div>
   );
